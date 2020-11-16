7393e7393b1d706bd60182ae42b21d9b62298fbb
João Apostulo <japostulo@gmail.com>
2020-11-16 13:38:44 -0300

    Cria o arquivo tailwind.config.js Feito a criação do arquivo e adicionado a fontSize de 7x1 com 8rem

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  theme: {
+    fontSize: {
+       '7xl': '8rem',
+    }
+  }
+}
