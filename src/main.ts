25a5499ffa8023f4db5422a5a9cebc5d3113deae
Yoharu <jonathas_a.a@hotmail.com>
2020-11-15 22:59:07 -0300

    Cria a estrututa inicial do programa Feita a criação da estrutura padrão do Vue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Axios from 'axios'
 
+Axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL;
+Vue.prototype.$axios = Axios;
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <video :src="`http://localhost:8000/api/video/${id}`" controls="true"></video>
+    <input style="padding:5px;border:1px solid blue" type="text" v-model="id">
   </div>
 </template>
 
@@ -11,8 +13,16 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return{
+      id:'03588'
+    }
+  },
   components: {
     HelloWorld
+  },
+  created(){
+
   }
 }
 </script>
