7da512010d78f9eb105b81adf60652c2b19d6691
João Apostulo <japostulo@gmail.com>
2020-11-16 13:37:32 -0300

    Cria a pasta Views Adicionado a pasta Views

---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <video-c :id="id" :mode="'music'"/>
+        
+        <div class="top-0 absolute">
+            <div class="w-screen flex justify-center items-center " style="height:95vh">
+                <div class="text-center text-white">
+                    <div class="text-6xl">
+                        {{id}}
+                    </div>
+                    <div class="text-5xl">
+                        {{name}}
+                    </div>
+                </div>
+            </div>
+
+            <div class="text-right h-full text-3xl text-white" style="height:5vh">
+                Coin: {{coin}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import videoC from '../components/Video'
+
+export default {
+    components:{
+        videoC,
+    },
+    data() {
+        return {
+            id:'00000',
+            name:'Digite uma música',
+            coin:0,
+            err:'',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Teste.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+        <div class="static bg-gray-600">
+            Static parent
+            <div class="absolute top-0 bg-gray-800">
+                Absolute child
+            </div>
+        </div>  
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
