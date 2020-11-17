443f20300e6f8a263daf44eacc501f18d2555eba
João Apostulo <japostulo@gmail.com>
2020-11-16 23:56:01 -0300

    Adiciona classes personalizadas no tailwind Adicionado classes de font-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,16 @@ module.exports = {
   plugins: [],
   theme: {
     fontSize: {
-       '7xl': '8rem',
+      'xl': '1.25rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+      '8xl': '6rem',
+      '9xl': '9rem',
+      '10xl': '14rem',
+
     }
   }
 }
